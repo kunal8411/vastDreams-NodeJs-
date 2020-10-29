2af6ad48f953ace46e1d519f9de9d1ecf3f062b5
Kunal Khairnar <you@example.comkunal.khairnar96@gmail.com>
2020-10-29 21:43:02 +0530

    changes for cors(working fine)

--- controller/booksController.js
+++ controller/booksController.js
@@ -4,7 +4,7 @@ const Books = require('../models/books');
 module.exports.getAllBooks= async function(req,res){
     var id ="5f93dade73d3673a55beea4e"
     const books = await Books.findOne({_id: id});
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8000/books');
     return res.json(200, {
        
         books:books.books
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const path= require('path');
 const port= 8000;
 const cookieParser= require('cookie-parser');
 var app = express();
+const cors = require('cors');
+app.use(cors())
 app.use(express.urlencoded());
 const expressLayout=require('express-ejs-layouts');
 app.use(cookieParser());
@@ -14,6 +16,8 @@ const mongoose= require('./config/mongoose');
 const ejs= require('ejs');
 
 
+
+  
 app.use(express.json());
 
 app.use('/', require('./routes/index'))
